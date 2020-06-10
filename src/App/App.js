a8933f7eb5a083371696e971dd03b8d3f4bb7634
John Fleming <johndeclanfleming@gmail.com>
2020-06-09 19:57:50 -0500

    nav link routing works

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,38 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import Auth from '../components/pages/Auth/Auth';
 import Home from '../components/pages/Home/Home';
 import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+import MyStuff from '../components/pages/MyStuff/MyStuff';
+import NewStuff from '../components/pages/NewStuff/NewStuff';
 
 import fbConnection from '../helpers/data/connection';
 
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 class App extends React.Component {
   state = {
     authed: false,
@@ -34,11 +57,22 @@ class App extends React.Component {
     const { authed } = this.state;
     return (
       <div className="App">
-        <MyNavbar authed={authed}></MyNavbar>
-        { authed
-          ? <Home authed={authed}/>
-          : <Auth authed={authed}></Auth>
-        }
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed}></MyNavbar>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PrivateRoute path='/home' component={Home} authed={authed}/>
+                  <PrivateRoute path='/stuff' component={MyStuff} authed={authed}/>
+                  <PrivateRoute path='/new' component={NewStuff} authed={authed}/>
+                  <PublicRoute path='/auth' component={Auth} authed={authed}/>
+                  <Redirect from="*" to="/home" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Home.scss';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
-        <img className="mt-3" src="https://i.redd.it/yl58vkjul2b41.jpg" alt="my stuff meme"/>
+      <div className="Home mx-auto">
+          <img className="mt-3" src="https://i.redd.it/yl58vkjul2b41.jpg" alt="my stuff meme"/>
       </div>
     );
   }
