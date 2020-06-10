484c74ba3b84af406550c86e4fc2c98f0d8956ab
John Fleming <johndeclanfleming@gmail.com>
2020-06-09 20:43:50 -0500

    state set with stuff array

--- src/components/pages/MyStuff/MyStuff.js
+++ src/components/pages/MyStuff/MyStuff.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import authData from '../../../helpers/data/authData';
+import stuffData from '../../../helpers/data/stuffData';
+
 class MyStuff extends React.Component {
+  state = {
+    stuff: [],
+  }
+
+  getStuff = () => {
+    const uid = authData.getUid();
+    stuffData.getStuffByUid(uid)
+      .then((stuff) => this.setState({ stuff }))
+      .catch((err) => console.error('could not get stuff: ', err));
+  }
+
+  componentDidMount() {
+    this.getStuff();
+  }
+
   render() {
     const editLink = '/edit/12345';
     const singleLink = '/stuff/12345';
     return (
-      <div className="MyStuff">
-        <h1 className="mt-3">My Stuff</h1>
+      <div className="MyStuff mx-auto">
+        <h1 className="my-3">My Stuff</h1>
         <Link className="btn btn-outline-dark mx-1" to={editLink}>Edit</Link>
         <Link className="btn btn-outline-dark mx-1" to={singleLink}>Single</Link>
       </div>
---
+++ src/helpers/data/stuffData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
+
+const getStuffByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/stuff.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      const fbStuff = response.data;
+      const stuff = [];
+      if (fbStuff !== null) {
+        Object.keys(fbStuff).forEach((stuffId) => {
+          fbStuff[stuffId].id = stuffId;
+          stuff.push(fbStuff[stuffId]);
+        });
+      }
+      resolve(stuff);
+    })
+    .catch((err) => resolve(err));
+});
+
+export default { getStuffByUid };
